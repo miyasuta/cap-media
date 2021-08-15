b372c118e6cc8ab4152579bec0533c219c6a124a
Mio Yasutake <mio.fujita.01@gmail.com>
2021-08-16 08:01:02 +0900

    fix media-service.js

--- srv/media-service.js
+++ srv/media-service.js
@@ -1,7 +1,4 @@
 module.exports = async function () {
-
-    const { Files } = cds.entities
-
     this.before('CREATE', 'Files', req => {
         console.log('Create called')
         console.log(JSON.stringify(req.data))
@@ -9,6 +6,7 @@ module.exports = async function () {
     })
 
     this.before('READ', 'Files', req => {
+        //check content-type
         console.log('content-type: ', req.headers['content-type'])
     })
 }
